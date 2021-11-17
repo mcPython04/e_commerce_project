d1b2ec28ee1f4bbf4e822e421f6f003b4921f430
mcPython04 <chen1911893611@gmail.com>
2021-11-17 17:35:13 -0600

    added basic menuing

---
+++ main.py
@@ -0,0 +1,38 @@
+
+def main():
+    print('Welcome to our cmd e-commerce shop!!!')
+
+    while True:
+        print('\n0: Login')
+        print('1: Create Account')
+        print('2: Quit program')
+
+        choice = input('Please select a menu option (enter the number): ')
+
+        if choice == '0':
+            username = input('Username: ')
+            pswd = input('Password: ')
+
+            # if everything works fine print other menu
+            while True:
+                print('0. View Books')
+                print('1. View Shirts')
+                print('2. Add Item to Cart')
+                print('3. Remove Item from Cart')
+                print('4. Checkout Item from Cart')
+                print('5. View Order History')
+                print('6. Edit Account')
+                print('7. Delete Account')
+                print('8. Logout')
+
+                option = input('Please select a menu option (enter the number): ')
+
+        elif choice == '1':
+            print('create account form here')
+
+        elif choice == '2':
+            break
+
+
+if __name__ == '__main__':
+    main()

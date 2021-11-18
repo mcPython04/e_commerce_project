b8811e7caba022b112ef614082656dcac8651ed9
mcPython04 <chen1911893611@gmail.com>
2021-11-18 13:46:38 -0600

    added connection to db; wrote code to log user in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import classes
+import mysql.connector
+import sys
 
 
 def main():
@@ -15,8 +17,13 @@ def main():
             username = input('Username: ')
             pswd = input('Password: ')
 
+            # try to get authentication by checking username and password through db
+            # then pass that return value to the while loop
+
+            flag = get_authentication(username, pswd)
+
             # if everything works fine print other menu
-            while True:
+            while flag:
                 print('0. View Books')
                 print('1. View Shirts')
                 print('2. Add Item to Cart')
@@ -29,12 +36,50 @@ def main():
 
                 option = input('Please select a menu option (enter the number): ')
 
+                if option == '8':
+                    print('Logging out......')
+                    flag = False
 
         elif choice == '1':
             print('create account form here')
 
         elif choice == '2':
             break
+
+
+# tries to see if such username or password exists in db
+# if exists instantiate the user object
+def get_authentication(username, password):
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="e_commerce"
+        )
+        print("Successful connection.")
+
+    except:
+        print("Failed connection.")
+        ## exits the program if unsuccessful
+        sys.exit()
+
+    cursor = connection.cursor()
+    query = "SELECT * FROM User WHERE username=%s AND password=%s"
+    param = (username, password)
+    cursor.execute(query, param)
+    result = cursor.fetchall()
+
+    if len(result) == 1:
+        # instantiate User Object here
+        # use result variable to instantiate User object
+        print("Logged in successful")
+        print(result)
+
+        return True
+    else:
+        print('Failed to Log in')
+        return False
 
 
 if __name__ == '__main__':

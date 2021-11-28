1d05425ac200040ac792e12fd82bfee27c14ee17
Jinhee <jy603@msstate.edu>
2021-11-28 08:21:58 -0600

    Finished user-class + set a DB server

--- classes.py
+++ classes.py
@@ -1,42 +1,99 @@
 import mysql.connector
+import pandas as pd
+import pymysql
+import logging
+import sshtunnel
+from sshtunnel import SSHTunnelForwarder
 import sys
+import main
+import paramiko
+from paramiko import SSHClient
+
+sql_hostname = 'localhost'
+sql_username = 'client_x'
+sql_password = 'SecretPassword7#'
+sql_main_database = 'shopping'
+sql_port = 3306
+#ssh_host = '192.168.0.71'
+ssh_host = '170.253.135.197'
+ssh_user = 'client'
+ssh_password = 'SecretPassword#'
+ssh_port = 22
 
 
-def User():
 
-    def __init__(self, userID, password):  # sign-in
+
+class User():
+
+    result = []
+    __userID = ""
+    __userName = ""
+    __password = ""
+    __email = ""
+    __contact = ""
+    __shippingAddress = ""
+    __creditNumber = ""
+    __billingAddress = ""
+
+    def login(self, userID, password):  # sign-in
         # connect to db
         try:
-            connection = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                password="",
-                database="e_commerce"
-            )
-            print("Successful connection.")
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
 
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"SELECT * FROM user WHERE userID = '{userID}' AND password= '{password}'"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully login')
+                        
+        
+                    except:
+                        print('Failed to login')
+                    
+                    conn.commit()
+                    conn.close
         except:
             print("Failed connection.")
             ## exits the program if unsuccessful
             sys.exit()
 
-        # retrieve user info from db
-        try:
-            cursor = connection.cursor()
-            query = 'INSERT INTO User (username, password, email, contact, shipping, credit, billing) ' \
-                    'VALUES (%s, %s, %s, %s, %s, %s, %s)'
-            param = (__userID, __userName, __password, __email, __contact,
-                    __shippingAddress, __creditNumber, __billingAddress)
-            cursor.execute(query, param)
-            connection.commit()
-            print(cursor.rowcount, " record inserted.")
-            print('Successfully created account')
-        except:
-            print('Failed to create account')
 
-            
+        # if username & password was found/correct instantiate user object and log user in
+        if len(result) == 1:
+            # instantiate User Object here
+            # use result variable to instantiate User object
+            print("Logged in successful")
 
-    def __init__(self, userID, userName, password, email, contact, shippingAddress="", creditNumber="", billingAddress=""):  # sign-up
+            # store tuple in 'flag' variable and grab user info
+            self.__userID = result['userID'][0] 
+            self.__userName= result['userName'][0]
+            self.__password = result['password'][0]
+            __email = result['email'][0]
+            print(f"###{__email}###")
+            self.__contact = result['contact'][0]
+            self.__shippingAddress = result['shippingAddress'][0]
+            self.__creditNumber = result['creditNumber'][0]
+            self.__billingAddress = result['billingAddress'][0]
+
+            self.authflag = True
+        else:
+            print('Failed to Log in')
+            self.authflag = False
+
+
+    def signup(self,userID, userName, password, email, contact, shippingAddress="", creditNumber="", billingAddress=""):  # sign-up
         # required feild : userID, userName, password, email, contact
 
         self.__userID = userID
@@ -48,46 +105,283 @@ def User():
         self.__creditNumber = creditNumber
         self.__billingAddress = billingAddress
 
-    # ask user for info. input
-    print('\nPlease fill out the form.')
-    __userID = input('Create a UserID: ')
-    __userName = input('Create a Username: ')
-    __password = input('Create a Password: ')
-    __email = input('Email: ')
-    __contact = input('Contact: ')
-    __shippingAddress = input('Shipping address: ')
-    __creditNumber = input('Credit card number: ')
-    __billingAddress = input('Billing address: ')
+        try:
 
-    # connect to db
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="e_commerce"
-        )
-        print("Successful connection.")
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                    ssh_username=ssh_user,
+                                    ssh_password=ssh_password,
+                                    remote_bind_address=(sql_hostname, sql_port)) as tunnel:
 
-    except:
-        print("Failed connection.")
-        ## exits the program if unsuccessful
-        sys.exit()
+                print("Trying to make a connection")
+                conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                        passwd=sql_password, db=sql_main_database,
+                        port=tunnel.local_bind_port)
 
-    # insert new user info into db
-    try:
-        cursor = connection.cursor()
-        query = 'INSERT INTO User (username, password, email, contact, shipping, credit, billing) ' \
-                'VALUES (%s, %s, %s, %s, %s, %s, %s)'
-        param = (__userID, __userName, __password, __email, __contact,
-                 __shippingAddress, __creditNumber, __billingAddress)
-        cursor.execute(query, param)
-        connection.commit()
-        print(cursor.rowcount, " record inserted.")
-        print('Successfully created account')
-    except:
-        print('Failed to create account')
+                print("made connection")
+                cur = conn.cursor(pymysql.cursors.DictCursor)
+                query = f"INSERT INTO user VALUES ('{userID}','{userName}','{password}','{email}','{contact}','{shippingAddress}','{creditNumber}','{billingAddress}');"
+                try:
+                    cur.execute(query)
+                    result = cur.fetchall()
+                    result = pd.DataFrame(result)
 
-    def __del__(self): print(f" Your account ({self.__userID}) is deleted. ")
+                    print(cur.rowcount, " record inserted.")
+                    print('Successfully created account')
+                                
+                except:
+                    print('Failed to create account')
+                
+                conn.commit()
+                conn.close
 
-    return
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()
+
+    
+
+    def set_pswd(self,changed_pswd):
+        try:
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"UPDATE user SET password = {changed_pswd} WHERE userID = '{self.__userID}';"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully update the password')
+                        
+                    except:
+                        print('Failed to update the password')
+                    
+                    self.__password=changed_pswd
+                    conn.commit()                    
+                    conn.close
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()
+
+    def set_email(self,changed_email):
+        try:
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"UPDATE user SET email = '{changed_email}' WHERE userID = '{self.__userID}';"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully update the email')
+                        
+                    except:
+                        print('Failed to update the email')
+                    
+                    self.__email=changed_email
+                    conn.commit()
+                    conn.close
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()
+
+    
+
+    def set_ship_addr(self,changed_ship_addr):
+        try:
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"UPDATE user SET shippingAddress = {changed_ship_addr} WHERE userID = '{self.__userID}';"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully update the ShippingAddress')
+                        
+                    except:
+                        print('Failed to update the ShippingAddress')
+                    
+                    self.__shippingAddress=changed_ship_addr
+                    conn.commit()
+                    conn.close
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()
+    
+    def set_bill_addr(self,changed_bill_addr):
+        try:
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"UPDATE user SET billingAddress = {changed_bill_addr} WHERE userID = '{self.__userID}';"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully update the billingAddress')
+                        
+                    except:
+                        print('Failed to update the billingAddress')
+                    
+                    self.__billingAddress=changed_bill_addr
+                    conn.commit()
+                    conn.close
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()
+    
+    def set_credit_num(self,changed_credit_num):
+        try:
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"UPDATE user SET creditNumber = {changed_credit_num} WHERE userID = '{self.__userID}';"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully update the creditNumber')
+                        
+                    except:
+                        print('Failed to update the creditNumber')
+                    
+                    self.__creditNumber=changed_credit_num
+                    conn.commit()
+                    conn.close
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()
+
+    def set_contact(self,changed_contact):
+        try:
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"UPDATE user SET contact = {changed_contact} WHERE userID = '{self.__userID}';"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully update the contact')
+                        
+                    except:
+                        print('Failed to update the contact')
+                    
+                    self.__contact=changed_contact
+                    conn.commit()
+                    conn.close
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()
+
+    def show_info(self):
+        print(f"\n  > userID: {self.__userID}"
+              f"\n  > userName: {self.__userName}"
+              f"\n  > email: {self.__email}"
+              f"\n  > contact: {self.__contact}"
+              f"\n  > shipping Address: {self.__shippingAddress}"
+              f"\n  > credit card Number: {self.__creditNumber}"
+              f"\n  > billing Address: {self.__billingAddress}\n")
+
+    def delete_accnt(self):
+        try:
+            with SSHTunnelForwarder((ssh_host, ssh_port),
+                                        ssh_username=ssh_user,
+                                        ssh_password=ssh_password,
+                                        remote_bind_address=(sql_hostname, sql_port)) as tunnel:
+
+                    print("Trying to make a connection")
+                    conn = pymysql.connect(host=sql_hostname, user=sql_username,
+                            passwd=sql_password, db=sql_main_database,
+                            port=tunnel.local_bind_port)
+
+                    print("made connection")
+                    cur = conn.cursor(pymysql.cursors.DictCursor)
+                    query = f"DELETE FROM user WHERE userID = '{self.__userID}';"
+                    try:
+                        cur.execute(query)
+                        result = cur.fetchall()
+                        result = pd.DataFrame(result)
+
+                        print('Successfully delete your account. Thank you!')
+                        
+                    except:
+                        print('Failed to delete your account')
+
+                    conn.commit()
+                    conn.close
+        except:
+            print("Failed connection.")
+            ## exits the program if unsuccessful
+            sys.exit()              
+
+    #def __del__(self): print(f" Your account ({self.__userID}) is logged out. ")
+
+
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import classes
 import mysql.connector
 import sys
+import pandas as pd
+import pymysql
+import logging
+import sshtunnel
+from sshtunnel import SSHTunnelForwarder
+import sys
 
 
 def main():
@@ -17,16 +23,18 @@ def main():
         choice = input('Please select a menu option (enter the number): ')
 
         if choice == '0':
-            username = input('Username: ')
+            userID = input('UserID: ')
             pswd = input('Password: ')
 
             # try to get authentication by checking username and password through db
             # then pass that return value to the while loop
 
-            flag = get_authentication(username, pswd)
+            user = classes.User()
+            user.login(userID, pswd)
+            auth_flag = user.authflag
 
             # if everything works fine print other menu
-            while flag:
+            while auth_flag:
                 print('What page would you like to go to?')
                 print('0. Books'
                       '\n1. Shirts'
@@ -34,7 +42,8 @@ def main():
                       '\n3. Cart'
                       '\n4. Logout')
 
-                option = input('Please select a menu option (enter the number): ')
+                option = input(
+                    'Please select a menu option (enter the number): ')
 
                 if option == '0':
                     print("Welcome to the Books page!")
@@ -44,78 +53,68 @@ def main():
                     print("Welcome to the Shirts page!")
                     print('Menu options: ')
 
-
                 elif option == '2':
-                    print("Account page: ")
-                    print('Menu options: ')
+                    print("Account page: 'Please select a menu option")
+                    print('0. Update the password'
+                            '\n1. Update your email'
+                            '\n2. Update your shipping address'
+                            '\n3. Update your billing address'
+                            '\n4. Update your credit card number'
+                            '\n5. Update your contact'
+                            '\n6. Show all my account information'
+                            '\n7. Delete your account')
+                    accnt_option= input("Enter the number : ")
 
+                    if accnt_option == '0':
+                        changed_pswd = input('Update your Password: ')
+                        user.set_pswd(changed_pswd)
+
+                    elif accnt_option == '1':
+                        changed_email = input('Update your email: ')
+                        user.set_email(changed_email)
+
+
+                    elif accnt_option == '2':
+                        changed_ship_addr = input('Update your shipping address: ')
+                        user.set_ship_addr(changed_ship_addr)
+
+
+                    elif accnt_option == '3':
+                        changed_bill_addr = input('Update your billing address: ')
+                        user.set_bill_addr(changed_bill_addr)
+
+                    elif accnt_option == '4':
+                        changed_credit_num = input('Update your credit card number: ')
+                        user.set_credit_num(changed_credit_num)
+
+                    elif accnt_option == '5':
+                        changed_contact = input('Update your contact: ')
+                        user.set_contact(changed_contact)
+
+                    elif accnt_option == '6':
+                        print("Here is your account information: ")
+                        user.show_info()
+
+                    elif accnt_option == '7':
+                        user.delete_accnt()
+                        auth_flag = False
 
                 elif option == '3':
                     print('Cart page: ')
                     print('Menu options: ')
 
-
                 elif option == '4':
+                    del user
                     print('Logging out......')
-                    flag = False
+                    auth_flag = False
 
         # calls function to create account
         elif choice == '1':
             create_account()
 
         # exits the program
-        elif choice == '2':
+        elif choice == '2':              
             break
-
-
-# tries to see if such username or password exists in db
-# if exists instantiate the user object
-def get_authentication(username, password):
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="e_commerce"
-        )
-        print("Successful connection.")
-
-    except:
-        print("Failed connection.")
-        ## exits the program if unsuccessful
-        sys.exit()
-
-    cursor = connection.cursor()
-    query = "SELECT * FROM User WHERE username=%s AND password=%s"
-    param = (username, password)
-    cursor.execute(query, param)
-    result = cursor.fetchall()
-
-    # if username & password was found/correct instantiate user object and log user in
-    if len(result) == 1:
-        # instantiate User Object here
-        # use result variable to instantiate User object
-        print("Logged in successful")
-
-        # store tuple in 'flag' variable and grab user info
-        flag = result[0]
-        userid = flag[0]
-        username = flag[1]
-        pswd = flag[2]
-        email = flag[3]
-        phone = flag[4]
-        shipping = flag[5]
-        credit_num = flag[6]
-        billing = flag[7]
-
-        # INSTANTIATE USER OBJECT HERE!!!!
-
-        print(result)
-
-        return True
-    else:
-        print('Failed to Log in')
-        return False
 
 
 # ask user for information and create account for user
@@ -123,6 +122,7 @@ def create_account():
 
     # ask user for info. input
     print('\nPlease fill out the form.')
+    userID = input('Create a UserID: ')
     username = input('Create a Username: ')
     pswd = input('Create a Password: ')
     email = input('Email: ')
@@ -131,33 +131,8 @@ def create_account():
     credit_num = input('Credit card number: ')
     billing = input('Billing address: ')
 
-    # connect to db
-    try:
-        connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="e_commerce"
-        )
-        print("Successful connection.")
-
-    except:
-        print("Failed connection.")
-        ## exits the program if unsuccessful
-        sys.exit()
-
-    # insert new user info into db
-    try:
-        cursor = connection.cursor()
-        query = 'INSERT INTO User (username, password, email, contact, shipping, credit, billing) ' \
-                'VALUES (%s, %s, %s, %s, %s, %s, %s)'
-        param = (username, pswd, email, phone, shipping, credit_num, billing)
-        cursor.execute(query, param)
-        connection.commit()
-        print(cursor.rowcount, " record inserted.")
-        print('Successfully created account')
-    except:
-        print('Failed to create account')
+    user = classes.User()
+    user.signup(userID, username, pswd, email, phone, shipping, credit_num, billing)
 
     return 0
 
